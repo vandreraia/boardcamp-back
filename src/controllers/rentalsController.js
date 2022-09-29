d11fd6cdb1beda11f3049807c3a21624b59cf125
Vandre <celiovandre@hotmail.com>
2022-09-29 15:58:33 -0300

    feat: get rentals now support query string

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -3,19 +3,30 @@ import connection from "../db/postgres.js";
 import dayjs from "dayjs";
 
 export async function getRentals(req, res) {
+    let { customerId, gameId } = req.query;
 
     try {
-        const { rows: rentals } = await connection.query('SELECT * FROM rentals');
-        let destruct= [];
+        let rentals;
+        if (customerId) {
+            const { rows } = await connection.query('SELECT * FROM rentals WHERE "customerId" = $1', [customerId]);
+            rentals = rows;
+        } else if (gameId) {
+            const { rows } = await connection.query('SELECT * FROM rentals WHERE "gameId" = $1', [gameId]);
+            rentals = rows;
+        } else {
+            const { rows } = await connection.query('SELECT * FROM rentals');
+            rentals = rows;
+        }
+
+        let destruct = [];
         for (let i = 0; i < rentals.length; i++) {
-            let {rows: customer} = await connection.query('SELECT id, name FROM customers WHERE id = $1', [rentals[i]["customerId"]]);
-            let {rows: game} = await connection.query('SELECT id, name, "categoryId" FROM games WHERE id = $1', [rentals[i]["gameId"]]);
+            let { rows: customer } = await connection.query('SELECT id, name FROM customers WHERE id = $1', [rentals[i]["customerId"]]);
+            let { rows: game } = await connection.query('SELECT id, name, "categoryId" FROM games WHERE id = $1', [rentals[i]["gameId"]]);
             game = game[0];
             customer = customer[0]
-            destruct.push({...rentals[i], customer, game})
-            console.log(destruct[i])
+            destruct.push({ ...rentals[i], customer, game })
         }
-        res.status(200).send(rentals);
+        res.status(200).send(destruct);
     } catch (error) {
         res.status(500).send(error.message);
     }
@@ -39,16 +50,16 @@ export async function postRentals(req, res) {
     }
 
     try {
-        const {rowCount: existUser} = await connection.query('SELECT * FROM customers WHERE id = $1', [customerId]);
-        const {rowCount: existGame} = await connection.query('SELECT * FROM games WHERE id = $1', [gameId]);
-        const {rows: gameInfo} = await connection.query('SELECT * FROM games WHERE id = $1', [gameId]);
-        const {rowCount: numberRentals} = await connection.query('SELECT * FROM rentals WHERE "gameId" = $1', [gameId]);
+        const { rowCount: existUser } = await connection.query('SELECT * FROM customers WHERE id = $1', [customerId]);
+        const { rowCount: existGame } = await connection.query('SELECT * FROM games WHERE id = $1', [gameId]);
+        const { rows: gameInfo } = await connection.query('SELECT * FROM games WHERE id = $1', [gameId]);
+        const { rowCount: numberRentals } = await connection.query('SELECT * FROM rentals WHERE "gameId" = $1', [gameId]);
         console.log(numberRentals);
         if (existUser === 0 || existGame === 0 || numberRentals > gameInfo[0]['stockTotal']) {
             return res.status(400).send("customer ou jogo nao exite ou nao tem mais estoque")
         }
 
-        const originalPrice = gameInfo[0]['pricePerDay']*daysRented;
+        const originalPrice = gameInfo[0]['pricePerDay'] * daysRented;
         await connection.query('INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "originalPrice") VALUES ($1, $2, $3, $4, $5)', [customerId, gameId, now, daysRented, originalPrice]);
         res.status(201).send("categoria criada com sucesso")
     } catch (error) {
